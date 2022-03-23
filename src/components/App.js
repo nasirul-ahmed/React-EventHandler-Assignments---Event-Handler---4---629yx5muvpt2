bf868faa714f27fdca68b00f13ec9cb6425c2155
nasiruahmedl369 <76066542+nasirul-ahmed@users.noreply.github.com>
2022-03-23 20:58:07 +0530

    m

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import { useRef } from "react/cjs/react.production.min";
+import "../styles/App.css";
 const App = () => {
 
-  const handleDoubleClick = (event) =>{
+  let count = 0;
+
   
-  }
+
+  const handleDoubleClick = (event) => {
+    event.preventDefault();
+    count += 1;
+
+    // if (count === 1) {
+    //   timeout = setTimeout(() => {
+    //     count = 0;
+    //     console.log("I was not double clicked");
+    //   }, 300);
+    // } 
+    // if(count === 2){
+    //   clearTimeout(timeout);
+    //   count = 0;
+    //   console.log("I was double clicked");
+    // }
+    
+    setTimeout(() => {
+      if (count === 1) {
+        console.log('I was not double clicked');
+      } else if (count === 2) {
+        console.log("I was double clicked");
+      }
+      count = 0;
+    }, 300);
+
+    
+  };
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button onClick={handleDoubleClick} id="dblclick-btn">
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
